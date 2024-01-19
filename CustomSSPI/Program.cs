d307931652aa86cddd5d62e22858889ad901b78e
Taylor Southwick <tasou@microsoft.com>
2024-01-19 11:10:10 -0800

    Add sample

--- CustomSSPI/Program.cs
+++ CustomSSPI/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Data.SqlClient;
+using System.Buffers;
+
+var connectionString = "Server=localhost;User ID=someUser;Password=somePassword;Integrated Security=SSPI;Initial Catalog=master";
+
+using var sqlconnection = new SqlConnection(connectionString)
+{
+    SSPIContextProviderFactory = () => new CustomSSPIProvider(),
+};
+
+await sqlconnection.OpenAsync();
+
+var command = new SqlCommand("SELECT name FROM master.dbo.sysdatabases", sqlconnection);
+using var result = await command.ExecuteReaderAsync();
+
+class CustomSSPIProvider : SSPIContextProvider
+{
+    protected override IMemoryOwner<byte> GenerateSspiClientContext(ReadOnlyMemory<byte> input)
+    {
+        throw new NotImplementedException();
+    }
+}
